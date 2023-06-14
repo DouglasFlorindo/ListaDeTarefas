b05106d3c82b7e52ce5823e515d22effabba1d92
Douglas Florindo <douglasflorindodeassis@gmail.com>
2023-06-13 22:28:46 -0300

    Update

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,2 +1,46 @@
+const lista = document.getElementById('lista');
 const botaoCriar = document.getElementById('botaoCriar');
 
+botaoCriar.addEventListener('click', criarTarefa);
+
+function criarTarefa() {
+
+    let conteudoTarefa = document.getElementById("inputTexto").value;
+
+    if (conteudoTarefa) {
+
+        let novoDiv = document.createElement('DIV');
+        let novoP = document.createElement('P');
+        let novoInput = document.createElement('INPUT');
+        let novoButton = document.createElement('BUTTON');
+        let novoSpan = document.createElement('SPAN');
+
+        novoDiv.className = "itemLista";
+        lista.appendChild(novoDiv);
+
+        novoInput.className = "checkBox";
+        novoInput.type = "checkBox";
+        novoInput.setAttribute("onchange", "marcarTarefa(this)");
+        novoDiv.appendChild(novoInput);
+
+        novoP.className = "itemLista";
+        novoP.textContent = conteudoTarefa;
+        novoDiv.appendChild(novoP);
+
+        novoButton.className = "botaoDeletar";
+        novoButton.setAttribute("onclick", "deletarTarefa(this)");
+        novoDiv.appendChild(novoButton);
+
+        novoSpan.className = "material-symbols-rounded";
+        novoSpan.textContent = "delete";
+        novoButton.appendChild(novoSpan);
+    }
+};
+
+function marcarTarefa() {
+
+};
+
+function deletarTarefa() {
+
+};

5d9d00b1222b5daa75dc4661118582c8fdb58f43
Douglas Florindo <douglasflorindodeassis@gmail.com>
2023-06-16 11:28:34 -0300

    Deus nos abandonou

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -2,6 +2,14 @@ const lista = document.getElementById('lista');
 const botaoCriar = document.getElementById('botaoCriar');
 const inputTexto = document.getElementById('inputTexto');
 
+if (navigator.language === 'pt-BR') {
+    document.title = 'Lista de Tarefas';
+    document.getElementById('titulo').textContent = 'Lista de Tarefas';
+    document.getElementById('inputTexto').setAttribute('placeholder', 'Insira a sua tarefa aqui...');  
+    document.getElementById('footer').textContent = 'Este site não salva as suas tarefas nem os seus dados.';
+    document.getElementById('configTexto').textContent = 'Escolha o seu tema:';
+};
+
 botaoCriar.addEventListener('click', criarTarefa);
 
 inputTexto.addEventListener('keypress', (event) => {
@@ -55,4 +63,22 @@ function marcarTarefa(elemento) {
 
 function deletarTarefa(elemento) {
     elemento.parentElement.remove();
+};
+
+function trocarTema(tema) {
+    let root = document.querySelector(':root');
+    let modal = document.getElementById('config');
+
+    switch (tema) {
+        case "1":
+            root.style.setProperty('--tema', 'linear-gradient(to right top, #e86284, #df75b6, #c38edf, #9ca7f7, #73bcff)');
+            root.style.setProperty('--destaque', '#ec2356');
+            modal.close();
+            break;
+        case "2":
+            root.style.setProperty('--tema', 'linear-gradient(to right top, #031542, #00427b, #006f8d, #009968, #55bc04)');
+            root.style.setProperty('--destaque', '#81A5FE');
+            modal.close();
+            break;
+    }
 };
